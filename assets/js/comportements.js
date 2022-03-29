86c5a460d1308b2ba5579a4ed9cd34dfc7549a10
GaboritJunior <47030574+GaboritJunior@users.noreply.github.com>
2022-03-29 12:17:26 +0200

    Opti

--- assets/js/comportements.js
+++
@@ -1,8 +0,0 @@
-const burgerId = document.getElementById('burger');
-
-function menu() {
-  const nav = document.getElementById('menu');
-  nav.style.width = '3vw';
-}
-
-burgerId.addEventListener('click', menu());
